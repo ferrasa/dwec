8c1c2e8baa1d5fcd13e83962b731da68ed569d20
ferrasa <ferrasa@gmail.com>
2025-04-23 18:27:57 -0300

    dwec

---
+++ editora/src/main/java/dw/editora/model/Artigo.java
@@ -0,0 +1,76 @@
+package dw.editora.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "artigo")
+public class Artigo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(nullable = false, length = 80)
+    private String titulo;
+    
+    @Column(nullable = false)
+    private String resumo;
+
+    @Column(columnDefinition = "BOOLEAN DEFAULT TRUE") 
+    private boolean publicado;
+      
+    public Artigo() {
+
+	}
+
+	public Artigo(String titulo, String resumo, boolean publicado) {
+		this.titulo = titulo;
+		this.resumo = resumo;
+		this.publicado = publicado;
+	}
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumo() {
+        return this.resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public boolean isPublicado() {
+        return this.publicado;
+    }
+
+    public void setPublicado(boolean publicado) {
+        this.publicado = publicado;
+    }
+
+    @Override
+    public String toString() {
+		return "Artigo [id=" + id + ", titulo=" + titulo + ", resumo=" + resumo + ", publicado=" + publicado + "]";
+	}
+
+
+}

372e85547d8c97b30d39020a2c36dfef8c19de3c
ferrasa <ferrasa@gmail.com>
2026-04-15 09:00:18 -0300

    dwec

--- editora/src/main/java/dw/editora/control/ArtigoController.java
+++ editora/src/main/java/dw/editora/control/ArtigoController.java
@@ -1,13 +1,111 @@
 package dw.editora.control;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dw.editora.model.Artigo;
 import dw.editora.repository.ArtigoRepository;
+
+
 
 @RestController
 public class ArtigoController {
     @Autowired
     ArtigoRepository rep;
+
+    /*
+     * GET / : listar todos os artigos
+     */
+    @GetMapping("/")
+    public ResponseEntity< List<Artigo> >  getAllArtigos(@RequestParam(required = false) String titulo){
+        try {
+            List<Artigo> la = new ArrayList<Artigo>();
+
+            if (titulo == null)
+                //retorna todos os artigos do BD
+                rep.findAll().forEach(la::add);
+            else
+                // retorna artigos aproximados por titulo
+                rep.findByTituloContainingIgnoreCase(titulo).forEach(la::add);
+
+            if (la.isEmpty())
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(la, HttpStatus.OK);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity< Artigo >  getArtigo(@PathVariable("id") long id){
+        try {
+            
+            Optional<Artigo> data =  rep.findById(id);
+
+            if (data.isPresent())
+            {
+                Artigo a = data.get();
+                return new ResponseEntity<>(a, HttpStatus.OK);
+            }
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /*
+     * POST / : salvar um artigo BD
+     */
+    @PostMapping("/")
+    public ResponseEntity<Artigo> createArtigo(@RequestBody Artigo ar)
+    {
+        try {
+            Artigo a = rep.save(new Artigo(ar.getTitulo(), ar.getResumo(), ar.isPublicado()));
+            return new ResponseEntity<>(a, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // TODO: handle exception
+             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /*
+     * PUT /:id : atualizar artigo dado um id
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Artigo> updateArtigo(@PathVariable("id") long id, @RequestBody Artigo a)
+    {
+        Optional<Artigo> data = rep.findById(id);
+
+        if (data.isPresent())
+        {
+            Artigo ar = data.get();
+            ar.setPublicado(a.isPublicado());
+            ar.setResumo(a.getResumo());
+            ar.setTitulo(a.getTitulo());
+
+            return new ResponseEntity<>(rep.save(ar), HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
 
 }
